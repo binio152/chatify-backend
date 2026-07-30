02068812f28a2fd827ace50018e4816b969c2169
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-07-30 11:03:23 +0700

    feat(middlewares): implement request validator

---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from "express";
+import { ZodType } from "zod";
+
+interface ValidateProps {
+  schema: ZodType<any>;
+  type: "body" | "query" | "params";
+}
+
+export const validateRequest = ({ type, schema }: ValidateProps) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req[type]);
+    console.log(result); // DEV ONLY
+
+    if (!result.success) {
+      const errorPaths = type === "body" ? "form-fields" : type;
+
+      const errors = Object.fromEntries(
+        result.error.issues.map((issue) => [
+          issue.path.at(-1) ?? errorPaths,
+          issue.message,
+        ]),
+      );
+
+      console.log(result.error.issues); // DEV ONLY
+
+      return res.status(400).json({ success: false, message: errors });
+    }
+
+    next();
+  };
+};
