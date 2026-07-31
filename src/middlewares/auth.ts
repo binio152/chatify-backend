8cfd1d58ee48ee967e8ceb8870110c32a181c156
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-07-31 09:30:36 +0700

    feat(middlewares): implement token validate middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError.ts";
+import { jwtVerify } from "../utils/auth.ts";
+
+export const authenticationToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) return next(new AppError("Access token is required.", 401));
+
+    const payload = jwtVerify(token);
+    console.log(payload); // DEV ONLY
+
+    req.user = payload;
+
+    next();
+  } catch (err) {
+    console.log("Error occured while validation authentication token", err);
+    next(new AppError("Invalid or expired access token.", 401));
+  }
+};
