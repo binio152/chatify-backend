fabf8b9f8b952eb3b3f31664ce2b8b24bbca3069
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-08-05 16:31:10 +0700

    refactor(cloudinary): organize Cloudinary uploads into separate folders

--- src/services/cloudinaryServices.ts
+++ src/services/cloudinaryServices.ts
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 export const cloudinaryServices = {
-  uploadAvtar: (fileBuffer: Buffer) => {
+  uploadUserAvtar: (fileBuffer: Buffer) => {
     return new Promise((resolve, reject) => {
       const stream = cloudinary.uploader.upload_stream(
         {
@@ -21,11 +21,48 @@ export const cloudinaryServices = {
     });
   },
 
-  sendMessageImage: (fileBuffer: Buffer) => {
+  uploadGroupAvtar: (fileBuffer: Buffer) => {
     return new Promise((resolve, reject) => {
       const stream = cloudinary.uploader.upload_stream(
         {
-          folder: "chatify/messages",
+          folder: "chatify/group",
+          resource_type: "image",
+          transformation: [
+            { width: 300, height: 300, crop: "fill", gravity: "auto" },
+          ],
+        },
+        (err, result) => {
+          if (err) return reject(err);
+          resolve(result);
+        },
+      );
+
+      stream.end(fileBuffer);
+    });
+  },
+
+  sendDirectImageMessage: (fileBuffer: Buffer) => {
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          folder: "chatify/messages/directs",
+          resource_type: "image",
+        },
+        (err, result) => {
+          if (err) return reject(err);
+          resolve(result);
+        },
+      );
+
+      stream.end(fileBuffer);
+    });
+  },
+
+  sendGroupImageMessage: (fileBuffer: Buffer) => {
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          folder: "chatify/messages/groups",
           resource_type: "image",
         },
         (err, result) => {
